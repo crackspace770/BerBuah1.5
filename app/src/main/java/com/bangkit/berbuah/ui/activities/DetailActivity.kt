d37df8ef90719e4231b9dde2140fce0efa14d281
nraliim <x.androtube@gmail.com>
2022-06-10 15:04:10 +0700

    add detail fruit to activity_detail.xml

--- app/src/main/java/com/bangkit/berbuah/ui/activities/DetailActivity.kt
+++ app/src/main/java/com/bangkit/berbuah/ui/activities/DetailActivity.kt
@@ -1,9 +1,19 @@
 package com.bangkit.berbuah.ui.activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.berbuah.databinding.ActivityDetailBinding
 
-class DetailActivity:AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
 
     companion object {
         const val EXTRA_STORY = "extra_story"
